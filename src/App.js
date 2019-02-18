b7da8df988d9f635678d02798a5c273b0ec85153
Banner Schafer <bannerbschafer@gmail.com>
2019-02-18 17:49:10 +1100

    Quick blindfold

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import ChessBoard from './Chess/Board/ui'
-import config from './config'
+import Stockfish from "./Chess/Stockfish/Stockfish";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ChessBoard fen={config.initialFen} />
+      <div>
+        <Stockfish />
       </div>
     );
   }
---
+++ src/Chess/Stockfish/Stockfish.jsx
@@ -0,0 +1,151 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Chess from "chess.js";
+import ChessBoard from "chessboardjsx";
+
+const HUMAN = "human";
+const STOCKFISH = "stockish";
+
+const propTypes = {
+  whitePlayerType: PropTypes.oneOf([HUMAN, STOCKFISH]),
+  blackPlayerType: PropTypes.oneOf([HUMAN, STOCKFISH]),
+  skill: PropTypes.number
+};
+
+const defaultProps = {
+  whitePlayerType: HUMAN,
+  blackPlayerType: STOCKFISH,
+  skill: 10
+};
+
+class Component extends React.Component {
+  state = {
+    game: new Chess(),
+    move: "",
+    cheat: false
+  };
+
+  componentWillMount() {
+    var wasmSupported =
+      typeof WebAssembly === "object" &&
+      WebAssembly.validate(
+        Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
+      );
+    var stockfish = new Worker(
+      wasmSupported ? "stockfish.wasm.js" : "stockfish.js"
+    );
+    stockfish.addEventListener("message", this.parseMessage);
+    stockfish.postMessage("uci");
+    this.setState({ engine: stockfish });
+  }
+
+  setOptions = () => {
+    const { engine } = this.state;
+    engine.postMessage(`setoption name Skill Level value ${this.props.skill}`);
+  };
+
+  requestMove = ponder => {
+    const { game, engine } = this.state;
+    const { whitePlayerType, blackPlayerType } = this.props;
+    const whiteToPlay = game.turn() === "w";
+
+    if (
+      (whiteToPlay && whitePlayerType === STOCKFISH) ||
+      (!whiteToPlay && blackPlayerType === STOCKFISH)
+    ) {
+      engine.postMessage(`position fen ${game.fen()}`);
+      engine.postMessage("go movetime 5000");
+    }
+  };
+
+  parseMessage = ({ data: message }) => {
+    const { engine, game } = this.state;
+
+    switch (true) {
+      case /^option/.test(message):
+      case /^id/.test(message):
+      case /^info/.test(message):
+      case /^\s*$/.test(message):
+        break;
+      case /^uciok$/.test(message):
+        this.setOptions();
+        engine.postMessage("isready");
+        break;
+      case /^readyok$/.test(message):
+        this.requestMove();
+        break;
+      case /^bestmove/.test(message):
+        const matches = message.match(
+          /^bestmove\s([a-z0-9]*)\sponder\s([a-z0-9]*)$/
+        );
+        const move = game.move(matches[1], { sloppy: true });
+        this.setState({ game: game, error: null, computerMove: move.san });
+        this.requestMove();
+        console.info(move);
+        break;
+      default:
+        console.info(message);
+    }
+  };
+
+  makeHumanMove = e => {
+    e.preventDefault();
+    const { move, game } = this.state;
+    const { blackPlayerType, whitePlayerType } = this.props;
+    const turn = game.turn();
+
+    if (turn === "b" && blackPlayerType !== HUMAN) {
+      return this.setState({ error: "it is not your move" });
+    }
+    if (turn === "w" && whitePlayerType !== HUMAN) {
+      return this.setState({ error: "it is not your move" });
+    }
+
+    const humanMove = game.move(move);
+    if (!humanMove) {
+      return this.setState({ error: "Unable to make move" });
+    }
+    this.setState({ error: null, move: "", game: game, computerMove: null });
+    this.requestMove();
+  };
+
+  updateMove = e => {
+    this.setState({ move: e.target.value });
+  };
+
+  render() {
+    const { move, error, computerMove, game, cheat } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.makeHumanMove}>
+          <input
+            name="userInput"
+            placeholder="Submit your move"
+            onChange={this.updateMove}
+            value={move}
+          />
+          <button style={{ display: "none" }} type="submit">
+            Move
+          </button>
+        </form>
+        <button
+          onClick={() => {
+            this.setState(state => ({
+              cheat: !state.cheat
+            }));
+          }}
+        >
+          {cheat ? "Hide" : "Cheat"}
+        </button>
+        {cheat ? <ChessBoard position={game.fen()} /> : null}
+        {computerMove ? <pre>{computerMove}</pre> : null}
+        {error ? <div>{error}</div> : null}
+      </div>
+    );
+  }
+}
+
+Component.propTypes = propTypes;
+Component.defaultProps = defaultProps;
+
+export default Component;
